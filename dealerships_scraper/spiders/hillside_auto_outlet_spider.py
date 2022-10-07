469003f480db998751390cb27d854b386f958a60
David Kim <dkhw90@gmail.com>
2022-10-07 21:50:56 +0200

    Start scraping queens auto

---
+++ dealerships_scraper/spiders/hillside_auto_outlet_spider.py
@@ -0,0 +1,2 @@
+
+# https://www.hillsideautooutlet.com/cars-for-sale-in-Jamaica-NY-Long-Island-New-Jersey/used_cars
---
+++ dealerships_scraper/spiders/queens_auto_mall_spider.py
@@ -0,0 +1,78 @@
+
+import scrapy
+from urllib.parse import urljoin
+import re
+import time
+import datetime
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.dirname('dealerships_scraper')))
+import items
+from spiders_utils import get_item_data_from_xpath
+
+
+class QueensAutoMallSpider(scrapy.Spider):
+  name = "johns_auto_group"
+  start_urls = [
+      'https://johnsautosales.com/newandusedcars?page=1'
+  ]
+
+  DEALERSHIP_INFO = {
+    'dealership_name': "Queeens Auto Mall",
+    'address': '134-01 Atlantic Ave',
+    'zipcode': '11418',
+    'city': 'Richmond Hill',
+    'state': 'NY'
+  }
+
+  def parse(self, response):
+    links = response.xpath("//a[@class='js-vehicle-item-link vehicle-item__link_no-decoration']/@href").extract()
+
+    if len(links) == 0:
+      return
+
+    for link in links:
+      url = f"https://www.queensauction.com{link}"
+      time.sleep(1)
+      yield scrapy.Request(url, callback=self.parse_car)
+
+    # curr_page = int(re.search('page=([0-9])+', response.url)[0].replace('page=', ''))
+    # next_page_url = re.sub('page=([0-9])+', f'page={curr_page + 1}', response.url)
+    # yield scrapy.Request(
+    #     url=next_page_url,
+    #     callback=self.parse
+    # )
+
+  def parse_car(self, response):
+    item = items.Car()
+
+    get_item_data_from_xpath(response, "//table[@class='fields']/tr/td[preceding-sibling::td[./div[@class='name_wrapper']/text()[contains(., 'Year')]]]/text()", item, 'year', 'int')
+    get_item_data_from_xpath(response, "//div[@class='col-sm-6']/p[@class='optMake']/text()", item, 'make', 'str', 1)
+    get_item_data_from_xpath(response, "//div[@class='col-sm-6']/p[@class='optModel']/text()", item, 'model', 'str', 1)
+    get_item_data_from_xpath(response, "//div[@class='col-sm-6']/p[@class='optTrim']/text()", item, 'trim', 'str', 1)
+    item['title'] = str(item['year']) + ' ' + str(item['make']) + ' ' + str(item['model']) + ' ' + str(item['trim'])
+    item['model_trim'] = str(item['model']) + ' ' + str(item['trim'])
+
+    get_item_data_from_xpath(response, "//div[@class='col-sm-6']/p[@class='optMileage']/text()", item, 'mileage', 'int', 1)
+    get_item_data_from_xpath(response, "//span[@class='lblPrice']/text()", item, 'price', 'int')
+
+    response.xpath("//table[@class='fields']/tr/td[preceding-sibling::td[./div[@class='name_wrapper']/text()[contains(., 'Year')]]]/text()").extract()
+
+
+    get_item_data_from_xpath(response, "//div[@class='col-sm-6']/p[@class='optTrans']/text()", item, 'transmission', 'str')
+    get_item_data_from_xpath(response, "//table[@class='fields']/tr/td[preceding-sibling::td[./div[@class='name_wrapper']/text()[contains(., 'VIN')]]]/text()", item, 'vin', 'str')
+    get_item_data_from_xpath(response, "//div[@class='col-sm-6']/p[@class='optEngine']/text()", item, 'engine', 'str', 1)
+    get_item_data_from_xpath(response, "//div[@class='col-sm-6']/p[@class='optInteriorColor']/text()", item, 'interior_color', 'str', 1)
+    get_item_data_from_xpath(response, "//div[@class='col-sm-6']/p[@class='optColor']/text()", item, 'exterior_color', 'str', 1)
+    get_item_data_from_xpath(response, "//div[@class='col-sm-6']/p[@class='optDrive']/text()", item, 'drivetrain', 'str', 1)
+    get_item_data_from_xpath(response, "//div[@class='col-sm-6']/p[@class='optType']/text()", item, 'vehicle_type', 'str', 1)
+
+    item['dealership_name'] = self.DEALERSHIP_INFO['dealership_name']
+    item['dealership_address'] = self.DEALERSHIP_INFO['address']
+    item['dealership_zipcode'] = self.DEALERSHIP_INFO['zipcode']
+    item['dealership_city'] = self.DEALERSHIP_INFO['city']
+    item['dealership_state'] = self.DEALERSHIP_INFO['state']
+    item['scraped_url'] = response.url
+    item['scraped_date'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    yield item
